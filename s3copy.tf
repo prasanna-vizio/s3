9ea6e14d403ab729f5dbcdb3ffdb9bc42c33d2ab
prasanna-vizio <167919134+prasanna-vizio@users.noreply.github.com>
2024-07-19 20:36:33 +0530

    Update s3copy.tf

--- s3copy.tf
+++ s3copy.tf
@@ -1,18 +1,30 @@
-variable "source_bucket" {
-  default = "testbucket-18072024-1"
+# scripts
+resource "aws_s3_object_copy" "copy_scripts" {
+  for_each = toset(var.objects_to_copy_scripts)
+  bucket        = var.destination_scripts
+  key           = each.value
+  source        = "${var.source_scripts}/${each.value}"
+}
+# spark-ui-logs
+resource "aws_s3_object_copy" "copy_spark-ui-logs" {
+  for_each = toset(var.objects_to_copy_spark-ui-logs)
+  bucket        = var.destination_spark-ui-logs
+  key           = each.value
+  source        = "${var.source_spark-ui-logs}/${each.value}"
 }
 
-variable "destination_bucket" {
-  default = "testbucket-18072024-2"
+# staging
+resource "aws_s3_object_copy" "copy_staging" {
+  for_each = toset(var.objects_to_copy_staging)
+  bucket        = var.destination_staging
+  key           = each.value
+  source        = "${var.source_staging}/${each.value}"
 }
 
-variable "object_key" {
-  default = "folders/test.txt"
-}
-
-# Copy the object from the source bucket to the destination bucket
-resource "aws_s3_object_copy" "copy_object" {
-  bucket        = var.destination_bucket
-  key           = var.object_key
-  source        = "${var.source_bucket}/${var.object_key}"
+# temp
+resource "aws_s3_object_copy" "copy_temp" {
+  for_each = toset(var.objects_to_copy_temp)
+  bucket        = var.destination_temp
+  key           = each.value
+  source        = "${var.source_temp}/${each.value}"
 }
